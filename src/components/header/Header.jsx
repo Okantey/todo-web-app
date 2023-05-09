355860eed0eae4dec35e9a53b597b75e03ffbcbb
nii_okantey <nunoogideon19@gmail.com>
2023-05-09 18:25:57 +0000

    updates

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
+import darkIcon from "../../images/icon-moon.svg";
+import lightIcon from "../../images/icon-sun.svg";
+import darkBg from "../../images/bg-desktop-dark.jpg";
+import lightBg from "../../images/bg-desktop-light.jpg";
 import "./Header.css";
 
 const Header = () => {
-  return <div>Header</div>;
+  const [theme, setTheme] = useState(false);
+  return (
+    <header
+      className="todo__header"
+      style={{
+        background: `url(${lightBg})`,
+        backgroundPosition: "center",
+        backgroundSize: "cover",
+        height: "40vh",
+      }}
+    >
+      <section className="header__content">
+        <div className="header__top">
+          <h1>TODO</h1>
+          <button className="theme__switch" title="theme">
+            <img src={darkIcon} alt="dark" />
+          </button>
+        </div>
+        <form className="header__down">
+          <label htmlFor="todo"></label>
+          <input type="text" name="todo" placeholder="Add Todo..." />
+        </form>
+      </section>
+    </header>
+  );
 };
 
 export default Header;
